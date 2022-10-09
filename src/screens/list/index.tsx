c42a1ea8f74cbecca8396be7ca8f80d9e70518fd
Yuvraj Misal <yuvraj.misal89@gmail.com>
2022-10-09 13:05:45 +0400

    rename item component to list_item

--- src/screens/list/index.tsx
+++ src/screens/list/index.tsx
@@ -3,7 +3,7 @@ import {ScrollView} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 import ListData from '../../utils/fake-data';
-import {ListItem} from './components/item';
+import {ListItem} from './components/list_item';
 
 //
 //
